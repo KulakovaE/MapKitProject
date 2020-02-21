3e7621fe606a72e98a3ff1077feca551698ac51f
Elena Kulakova <egeorgievska89@gmail.com>
2020-02-21 12:27:59 +0100

    Initial Commit

---
+++ MapKitProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapKitProject
+//
+//  Created by Elena Kulakova on 2020-02-21.
+//  Copyright © 2020 Elena Kulakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
